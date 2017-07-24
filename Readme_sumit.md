d005ed42e40cf9c38dc4c4bbc25b67df58572bda
Sumit Nagal <sumit_nagal@intuit.com>
2017-07-24 14:45:00 -0700

    first commit

---
+++ Readme_sumit.md
@@ -0,0 +1,18 @@
+```
+  viewer {
+    id
+    widgets{
+      edges{
+        node{
+          id
+          name
+          description
+          color
+          size
+          qty
+        }
+      }
+    }
+  }
+}
+```
--- server/types/viewer-type.js
+++ server/types/viewer-type.js
@@ -1,10 +1,15 @@
-import { GraphQLObjectType, GraphQLString } from 'graphql';
-import { globalIdField } from 'graphql-relay';
+import { GraphQLObjectType } from 'graphql';
+import { globalIdField, connectionArgs, connectionFromArray } from 'graphql-relay';
 
 import { nodeInterface } from '../utils/node-definitions';
 import { registerType } from '../utils/resolve-type';
+import { WidgetData } from '../models/widget-data'
 
-import { Viewer } from '../models/graphql-models';
+import { Viewer, Widget } from '../models/graphql-models';
+
+import { widgetConnectionType } from '../connection/widgets';
+
+
 
 export const viewerType = new GraphQLObjectType({
 
@@ -12,7 +17,20 @@ export const viewerType = new GraphQLObjectType({
 
   fields: () => ({
     id: globalIdField('Viewer'),
-    message: { type: GraphQLString }, 
+    widgets: {
+      type: widgetConnectionType,
+      args: connectionArgs,
+      resolve: (_1,args, { baseUrl }) => {
+        const widgetData = new WidgetData(baseUrl);
+        return widgetData.all().then(widgets => { 
+          const widgetModels = widgets.map( w=>
+          Object.assign(new Widget(), w ));
+          return connectionFromArray(widgetModels, args);
+        });
+
+      },
+
+    }
   }),
 
   interfaces: () => [nodeInterface]
