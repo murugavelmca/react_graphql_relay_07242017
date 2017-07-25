37c68d1f9682b987d2775e805bca41e6b9d5ba79
Sumit Nagal <sumit_nagal@intuit.com>
2017-07-25 16:24:41 -0700

    added server side code for mutation

---
+++ server/types/mutation-type.js
@@ -0,0 +1,10 @@
+import { GraphQLObjectType } from 'graphql';
+import { insertWidgetMutationType } from './insert-widget-mutation-type';
+
+
+export const mutationType = new GraphQLObjectType ({
+  name: 'Mutation',
+  fields: () => ({
+    insertWidget: insertWidgetMutationType,
+  }),
+});
